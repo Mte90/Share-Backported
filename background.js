59c4893848b1d458b17accd7e37c0808d05ca60d
Sergey Ivanov <xufocoder@gmail.com>
2017-10-01 00:27:58 +0300

    Fix codestyle errors:  * Identifier 'sb_id' is not in camel case.  * Identifier 'sb_prev_url' is not in camel case.

--- background.js
+++ background.js
@@ -1,24 +1,24 @@
 /* global browser */
 
-var sb_id, sb_prev_url
+var sbId, sbPrevUrl
 // Create the window and save the tab id
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.type === 'share-backid') {
     browser.windows.create(request.data).then(function (tab) {
-      sb_id = tab.id
-      sb_prev_url = request.data.url
+      sbId = tab.id
+      sbPrevUrl = request.data.url
       return tab.id
     })
   }
 })
 // Autoclose the iwndow when the url change
 browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (sb_id === tab.windowId) {
-    if (sb_prev_url.indexOf('https://www.linkedin.com/shareArticle') >= 0 || sb_prev_url.indexOf('https://plus.google.com/share') >= 0 || sb_prev_url.indexOf('https://reddit.com/') >= 0) {
+  if (sbId === tab.windowId) {
+    if (sbPrevUrl.indexOf('https://www.linkedin.com/shareArticle') >= 0 || sbPrevUrl.indexOf('https://plus.google.com/share') >= 0 || sbPrevUrl.indexOf('https://reddit.com/') >= 0) {
       browser.tabs.insertCSS(tabId, {code: 'body { overflow: auto !important; }'})
     }
     browser.tabs.get(tabId, function (tabinfo) {
-      if (sb_prev_url !== tabinfo.url) {
+      if (sbPrevUrl !== tabinfo.url) {
         var close = false
         if (tabinfo.url.indexOf('dialog/return/close#_=_') > 0 || tabinfo.url.indexOf('latest_status_id=') > 0 || tabinfo.url === 'https://plus.google.com/') {
           close = true
