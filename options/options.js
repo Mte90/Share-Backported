5d03e905be452be78c1c4caff2ef027298901717
Daniele Scasciafratte <mte90net@gmail.com>
2018-01-28 18:07:46 +0100

    new interface with new code done

--- options/options.js
+++ options/options.js
@@ -4,32 +4,27 @@ function saveOptions(e) {
   e.preventDefault();
   var parameters = {};
   document.querySelectorAll('input[id]').forEach(function(item) {
-    if (item.type === 'checked') {
+    if (item.type === 'checkbox') {
       parameters[item.id] = item.checked;
     } else {
       parameters[item.id] = item.value;
     }
   });
-  console.log(parameters)
   browser.storage.local.set(parameters);
 }
 
 function restoreOptions() {
-  var shares = ['facebook', 'twitter', 'google', 'linkedin', 'tumblr', 'reddit', 'telegram', 'gmail', 'diaspora', 'vkontakte', 'yahoo', 'pinboard', 'outlook', 'pageaction'];
-  shares.forEach(function(item) {
-    var getting = browser.storage.local.get(item);
+  var value = '';
+  var getting = '';
+  document.querySelectorAll('input[id]').forEach(function(item) {
+    item = item.id;
+    getting = browser.storage.local.get(item);
     getting.then(function(result) {
-      document.querySelector('#' + item).checked = result[Object.keys(result)[0]];
-    }, function(error) {
-      console.log(`Error: ${error}`);
-    });
-  });
-  var shares = ['shaarli'];
-  shares.forEach(function(item) {
-    var getting = browser.storage.local.get(item);
-    getting.then(function(result) {
-      if (result[Object.keys(result)[0]] !== undefined) {
-        document.querySelector('#' + item).value = result[Object.keys(result)[0]];
+      value = result[Object.keys(result)[0]];
+      if (Number.isInteger(value)) {
+        document.querySelector('#' + item).value = value;
+      } else {
+        document.querySelector('#' + item).checked = value;
       }
     }, function(error) {
       console.log(`Error: ${error}`);
