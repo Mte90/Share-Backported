ec728ed2b0029ddf7a88d389ba11a68474f7b04b
Daniele Scasciafratte <mte90net@gmail.com>
2018-01-28 17:50:59 +0100

    working on a settings panel

--- options/options.js
+++ options/options.js
@@ -1,46 +1,39 @@
-/* global browser, document */
+/* global browser, document, console */
 
 function saveOptions(e) {
   e.preventDefault();
-  browser.storage.local.set({
-	facebook: document.querySelector('#facebook').checked,
-	twitter: document.querySelector('#twitter').checked,
-	google: document.querySelector('#google').checked,
-	linkedin: document.querySelector('#linkedin').checked,
-	tumblr: document.querySelector('#tumblr').checked,
-	reddit: document.querySelector('#reddit').checked,
-	telegram: document.querySelector('#telegram').checked,
-	gmail: document.querySelector('#gmail').checked,
-	diaspora: document.querySelector('#diaspora').checked,
-	vkontakte: document.querySelector('#vkontakte').checked,
-	yahoo: document.querySelector('#yahoo').checked,
-	pinboard: document.querySelector('#pinboard').checked,
-    outlook: document.querySelector('#outlook').checked,
-	shaarli: document.querySelector('#shaarli').value,
-	pageaction: document.querySelector('#pageaction').checked
+  var parameters = {};
+  document.querySelectorAll('input[id]').forEach(function(item) {
+    if (item.type === 'checked') {
+      parameters[item.id] = item.checked;
+    } else {
+      parameters[item.id] = item.value;
+    }
   });
+  console.log(parameters)
+  browser.storage.local.set(parameters);
 }
 
 function restoreOptions() {
-  var shares = ['facebook', 'twitter', 'google', 'linkedin', 'tumblr', 'reddit', 'telegram', 'gmail', 'diaspora', 'vkontakte', 'yahoo','pinboard', 'outlook', 'pageaction'];
-  shares.forEach(function (item) {
-	var getting = browser.storage.local.get(item);
-	getting.then(function (result) {
-	  document.querySelector('#' + item).checked = result[Object.keys(result)[0]];
-	}, function (error) {
-	  console.log(`Error: ${error}`);
-	});
+  var shares = ['facebook', 'twitter', 'google', 'linkedin', 'tumblr', 'reddit', 'telegram', 'gmail', 'diaspora', 'vkontakte', 'yahoo', 'pinboard', 'outlook', 'pageaction'];
+  shares.forEach(function(item) {
+    var getting = browser.storage.local.get(item);
+    getting.then(function(result) {
+      document.querySelector('#' + item).checked = result[Object.keys(result)[0]];
+    }, function(error) {
+      console.log(`Error: ${error}`);
+    });
   });
   var shares = ['shaarli'];
-  shares.forEach(function (item) {
-	var getting = browser.storage.local.get(item);
-	getting.then(function (result) {
-	  if (result[Object.keys(result)[0]] !== undefined) {
-		document.querySelector('#' + item).value = result[Object.keys(result)[0]];
-	  }
-	}, function (error) {
-	  console.log(`Error: ${error}`);
-	});
+  shares.forEach(function(item) {
+    var getting = browser.storage.local.get(item);
+    getting.then(function(result) {
+      if (result[Object.keys(result)[0]] !== undefined) {
+        document.querySelector('#' + item).value = result[Object.keys(result)[0]];
+      }
+    }, function(error) {
+      console.log(`Error: ${error}`);
+    });
   });
 }
 
