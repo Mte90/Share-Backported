3e3ba9743c27217d6c8afa739be75693cc09c941
Daniele Scasciafratte <mte90net@gmail.com>
2020-10-20 16:21:01 +0200

    fix(autoclose): reddit support, #125

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 var sbId, sbPrevUrl;
 // Create the window and save the tab id
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.type === 'share-backid') {
+  if (request.type === "share-backid") {
     browser.windows.create(request.data).then(function (tab) {
       sbId = tab.id;
       sbPrevUrl = request.data.url;
       return tab.id;
     });
-  } else if (request.type === 'share-backid-container') {
+  } else if (request.type === "share-backid-container") {
     browser.tabs.create(request.data).then(function (tab) {
       sbId = tab.id;
       sbPrevUrl = request.data.url;
@@ -17,12 +17,12 @@ browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 // Autoclose the window when the url change
-browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (sbId === tab.windowId) {
     const cssInjectionsFor = [
-      'https://www.linkedin.com/shareArticle',
-      'https://www.reddit.com/',
-      'shaarli'
+      "https://www.linkedin.com/shareArticle",
+      "https://www.reddit.com/",
+      "shaarli",
     ];
 
     if (sbPrevUrl !== undefined) {
@@ -33,61 +33,35 @@ browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
       if (shallInjectCss) {
         browser.tabs.insertCSS(tabId, {
-          code: 'body { overflow: auto !important; }'
+          code: "body { overflow: auto !important; }",
         });
       }
     }
 
-    if (shallObserveTitle(sbPrevUrl)) {
-      titleChangeStrategy(tabId);
-    } else {
-      urlChangeStrategy(tabId, sbPrevUrl);
-    }
- }
+    urlChangeStrategy(tabId, sbPrevUrl);
+  }
 });
 
-function shallObserveTitle (shareUrl) {
-  // TODO: Shared data with modal.html
-  const urls = [
-    'https://www.reddit.com/submit',
-  ];
+function urlChangeStrategy(tabId, sbPrevUrl) {
+  const closeWhen = ["dialog/close_window", "latest_status_id="];
 
-  return urls.some((url) => shareUrl.startsWith(url));
-}
-
-function titleChangeStrategy (tabId) {
-  const closeWhen = [
-    // FIXME: Replace with meaningful heuristic!
-    'Log in'
-  ];
-
-  browser.tabs.get(tabId, function (tabinfo) {
-    const modalTitle = tabinfo.title;
-
-    // True if any of above parts are part of the modal URL
-    const shallClose = closeWhen.some(function (titlePart) {
-      return modalTitle.includes(titlePart);
-    });
-
-    if (shallClose) {
-      browser.tabs.remove(tabId);
-    }
-  });
-}
-
-function urlChangeStrategy (tabId, sbPrevUrl) {
-  const closeWhen = [
-    'dialog/close_window',
-    'latest_status_id='
+  const closeCarefullyWhen = [
+    "reddit.com/user/.+/comments/.+/",
+    "reddit.com/r/.+/comments/.+/",
   ];
 
   browser.tabs.get(tabId, function (tabinfo) {
     const modalUrl = tabinfo.url;
 
-    if (sbPrevUrl !== modalUrl && modalUrl !== 'about:blank') {
+    if (sbPrevUrl !== modalUrl && modalUrl !== "about:blank") {
       // True if any of above parts are part of the modal URL
-      const shallClose = closeWhen.some(function (urlPart) {
+      var shallClose = closeWhen.some(function (urlPart) {
         return modalUrl.includes(urlPart);
+      });
+
+      shallClose = closeCarefullyWhen.some(function (regex) {
+        var testUrl = RegExp(regex);
+        return testUrl.test(modalUrl);
       });
 
       if (shallClose) {
@@ -100,45 +74,51 @@ function urlChangeStrategy (tabId, sbPrevUrl) {
 // call function which get the address bar color (toolbar_field)
 setPageActionIcon();
 
-async function setPageActionIcon () {
+async function setPageActionIcon() {
   var themeInfo = await browser.theme.getCurrent();
-  let color = 'light';
-  var rgb_list = ['rgb(71, 71, 73)', 'rgb(50, 50, 52)']; // list of RGB where icon_theme should be light
+  let color = "light";
+  var rgb_list = ["rgb(71, 71, 73)", "rgb(50, 50, 52)"]; // list of RGB where icon_theme should be light
   if (themeInfo.colors && themeInfo.colors.toolbar_field) {
     if (!rgb_list.includes(themeInfo.colors.toolbar_field.toString())) {
-      color = 'dark';
+      color = "dark";
     }
   }
   setPageAction(color);
 }
 
-function setPageAction (color) {
-  var ext_icon = 'icon-' + color + '.svg';
+function setPageAction(color) {
+  var ext_icon = "icon-" + color + ".svg";
   browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    var getting = browser.storage.local.get('pageaction');
-    getting.then(function (result) {
-      var pageaction = result.pageaction;
-      if (!pageaction) {
-        if (tab.url.indexOf('about:') !== 0 && tab.url.indexOf('moz-extension:') !== 0) {
-          browser.pageAction.setIcon({
-            tabId: tab.id,
-            path: ext_icon
-          });
+    var getting = browser.storage.local.get("pageaction");
+    getting.then(
+      function (result) {
+        var pageaction = result.pageaction;
+        if (!pageaction) {
+          if (
+            tab.url.indexOf("about:") !== 0 &&
+            tab.url.indexOf("moz-extension:") !== 0
+          ) {
+            browser.pageAction.setIcon({
+              tabId: tab.id,
+              path: ext_icon,
+            });
 
-          browser.pageAction.setTitle({
-            tabId: tab.id,
-            title: 'Share'
-          });
+            browser.pageAction.setTitle({
+              tabId: tab.id,
+              title: "Share",
+            });
 
-          browser.pageAction.show(tab.id);
-          browser.pageAction.setPopup({
-            tabId,
-            popup: '/modal/modal.html'
-          });
+            browser.pageAction.show(tab.id);
+            browser.pageAction.setPopup({
+              tabId,
+              popup: "/modal/modal.html",
+            });
+          }
         }
+      },
+      function (error) {
+        console.log(`Error: ${error}`);
       }
-    }, function (error) {
-      console.log(`Error: ${error}`);
-    });
+    );
   });
 }
